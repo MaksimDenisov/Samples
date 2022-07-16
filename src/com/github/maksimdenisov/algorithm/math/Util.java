176f6b5a75246f753d84ce0e83fe8c024adb3692
max <max.nick.demax@gmail.com>
2022-07-16 10:23:55 +0300

    add algorithm of rapid exponentiation.

--- src/com/github/maksimdenisov/algorithm/math/Util.java
+++ src/com/github/maksimdenisov/algorithm/math/Util.java
@@ -4,10 +4,21 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Util {
+
     public static long getNumber() {
         while (true) {
             try {
                 return new Scanner(System.in).nextLong();
+            } catch (NoSuchElementException e) {
+                System.out.println("Please enter number");
+            }
+        }
+    }
+
+    public static double getDoubleNumber() {
+        while (true) {
+            try {
+                return new Scanner(System.in).nextDouble();
             } catch (NoSuchElementException e) {
                 System.out.println("Please enter number");
             }
